a76944bc9fe45e913e395862c7e3314a9856fff1
gunesyu <gunesyunusemre@gmail.com>
2015-11-28 18:21:01 +0200

    images added

--- jquery interactive map/app.js
+++ jquery interactive map/app.js
@@ -13,10 +13,12 @@ $(document).ready(function(){
 		$("a.dot").removeClass("selected");
 		$(this).addClass("selected");
 
-		/*var city = ".city_detail#"+$(this).attr("city");
+		var city = ".city_detail#"+$(this).attr("city");
+		var htmlCode = $(city).html();
+
 		$(".detail_container").fadeOut(500, function(){
-			$(".detail_container .city_detail").html($(city).html());
+			$(".detail_container .city_detail").html(htmlCode);
 			$(".detail_container").fadeIn(500);
-		});*/
+		});
 	});
 });
